cb60132c293907fd1ec285f6e88b208b6f2febbe
harurunrunrun <68173582+harurunrunrun@users.noreply.github.com>
2020-12-27 13:45:04 +0900

    Add files via upload

---
+++ library/BIT.cpp
@@ -0,0 +1,18 @@
+int bit[1000001],n;
+
+int sum(int i){
+  int s=0;
+	while(i>0){
+	  s+=bit[i];
+		i-=i&-i;
+	}
+	return s;
+}
+
+void add(int i,int x){
+	while(i<=n){
+	  bit[i]+=x;
+		i+=i&-i;
+	}
+	return;
+}
