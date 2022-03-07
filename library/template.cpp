253e31464e55477e746a0115d901400dcb67ae9a
harurun <68173582+harurunrunrun@users.noreply.github.com>
2022-03-07 22:11:29 +0900

    Create template.cpp

---
+++ library/template.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <deque>
+#include <queue>
+#include <string>
+#include <iomanip>
+#include <set>
+#include <unordered_set>
+#include <map>
+#include <unordered_map>
+#include <utility>
+#include <stdio.h>
+#include <math.h>
+#include <assert.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+using ll=long long;
+
+int main(){
+  
+}
+
+
