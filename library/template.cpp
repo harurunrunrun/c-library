85cc3362c4399a5eff980b09409d6e3f593d36eb
harurun <68173582+harurunrunrun@users.noreply.github.com>
2022-06-22 22:31:48 +0900

    Update template.cpp

--- library/template.cpp
+++ library/template.cpp
@@ -33,6 +33,7 @@ using ll=long long;
 #define rep2d(i,j,H,W) for(ll (i)=0;(i)<(H);(i)++)for(ll (j)=0;(j)<(W);j++)
 #define is_in(x,y) (0<=(x) && (x)<H && 0<=(y) && (y)<W)
 #define yn {cout<<"Yes"<<endl;}else{cout<<"No"<<endl;}
+#define double_out(x) fixed << setprecision(x)
 inline ll powll(ll x,ll n){ll r=1;while(n>0){if(n&1){r*=x;};x*=x;n>>=1;};return r;}
 
 int main(){
